226e14133c4eecfd10db33b612da52a83ea8465f
yongliu <yongliu@jjldxz.com>
2019-03-26 19:30:00 +0800

    更新sql

--- install.sql
+++ install.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `questionare` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
-USE `questionare`;
 -- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
 --
 -- Host: 127.0.0.1    Database: questionare
@@ -33,13 +31,12 @@ CREATE TABLE `admin` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
---
 -- Dumping data for table `admin`
 --
 
 LOCK TABLES `admin` WRITE;
 /*!40000 ALTER TABLE `admin` DISABLE KEYS */;
-INSERT INTO `admin` VALUES (1,'admin','d033e22ae348aeb5660fc2140aec35850c4da997'),(2,'admin2','d033e22ae348aeb5660fc2140aec35850c4da997');
+INSERT INTO `admin` VALUES (1,'admin','d033e22ae348aeb5660fc2140aec35850c4da997');
 /*!40000 ALTER TABLE `admin` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -52,14 +49,14 @@ DROP TABLE IF EXISTS `naire`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `naire` (
   `n_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '问卷id',
-  `a_id` int(11) NOT NULL COMMENT '管理员id',
-  `n_create_time` varchar(14) NOT NULL COMMENT '创建时间',
+  `a_id` int(11) NOT NULL DEFAULT '0' COMMENT '管理员id',
   `n_deadline` varchar(14) NOT NULL COMMENT '截止时间',
   `n_title` varchar(255) NOT NULL COMMENT '问卷标题',
   `n_status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '发布状态',
   `n_intro` text COMMENT '问卷介绍',
+  `ctime` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`n_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='问卷表';
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='问卷表';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,6 +65,7 @@ CREATE TABLE `naire` (
 
 LOCK TABLES `naire` WRITE;
 /*!40000 ALTER TABLE `naire` DISABLE KEYS */;
+INSERT INTO `naire` VALUES (1,0,'1577721600000','倾听您的想法 满足您的需求',1,'非常感谢您对金吉列留学长期以来的大力支持与信任，为争取您的最大满意。特此倾听您对本届展会活动的看法、实际需求、建议和期望。快来说出你对我的小心思，让我准备准备满足你！','2019-03-26 19:11:15');
 /*!40000 ALTER TABLE `naire` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -84,8 +82,10 @@ CREATE TABLE `options` (
   `n_id` int(11) NOT NULL COMMENT '问卷ID',
   `q_id` int(11) NOT NULL COMMENT '题目ID',
   `o_isaddtion` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否有附加内容',
+  `del` tinyint(1) DEFAULT '0' COMMENT '删除时候为1',
+  `del_time` datetime DEFAULT NULL,
   PRIMARY KEY (`o_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='题目选项表';
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COMMENT='题目选项表';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -94,6 +94,7 @@ CREATE TABLE `options` (
 
 LOCK TABLES `options` WRITE;
 /*!40000 ALTER TABLE `options` DISABLE KEYS */;
+INSERT INTO `options` VALUES (1,'学生或家长',1,1,0,0,NULL),(2,'校方',1,1,0,0,NULL),(3,'内部员工',1,1,0,0,NULL),(4,'合作方',1,1,0,0,NULL),(5,'其他',1,1,1,0,NULL),(6,'不满意',1,2,0,0,NULL),(7,'一般',1,2,0,0,NULL),(8,'非常满意',1,2,0,0,NULL),(9,'参展院校',1,3,0,0,NULL),(10,'顾问服务',1,3,0,0,NULL),(11,'讲座安排',1,3,0,0,NULL),(12,'宣传方式',1,3,0,0,NULL),(13,'活动现场',1,3,0,0,NULL),(14,'其他',1,3,1,0,NULL);
 /*!40000 ALTER TABLE `options` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -113,8 +114,10 @@ CREATE TABLE `question` (
   `q_description` text COMMENT '问题描述',
   `c_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `u_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `del` tinyint(1) NOT NULL DEFAULT '0' COMMENT '删除 0位不删除 1为删除',
+  `del_time` datetime DEFAULT NULL COMMENT '删除时间',
   PRIMARY KEY (`q_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='题目表';
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='题目表';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -123,6 +126,7 @@ CREATE TABLE `question` (
 
 LOCK TABLES `question` WRITE;
 /*!40000 ALTER TABLE `question` DISABLE KEYS */;
+INSERT INTO `question` VALUES (1,'您的身份是？','单选',1,1,NULL,'2019-03-26 19:05:39','2019-03-26 19:05:39',0,NULL),(2,'您对本届展会是否满意','单选',1,1,NULL,'2019-03-26 19:05:39','2019-03-26 19:05:39',0,NULL),(3,'您对本届展会有哪些方面想要和我谈一谈？','单选',1,1,NULL,'2019-03-26 19:05:39','2019-03-26 19:05:39',0,NULL),(4,'请写下您的宝贵意见或遇见的问题吧。','文本',1,1,NULL,'2019-03-26 19:05:39','2019-03-26 19:05:39',0,NULL);
 /*!40000 ALTER TABLE `question` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -149,6 +153,31 @@ CREATE TABLE `result` (
 LOCK TABLES `result` WRITE;
 /*!40000 ALTER TABLE `result` DISABLE KEYS */;
 /*!40000 ALTER TABLE `result` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_question_relation`
+--
+
+DROP TABLE IF EXISTS `user_question_relation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_question_relation` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NOT NULL COMMENT '用户uid',
+  `nid` int(11) NOT NULL COMMENT '问卷id',
+  `c_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_question_relation`
+--
+
+LOCK TABLES `user_question_relation` WRITE;
+/*!40000 ALTER TABLE `user_question_relation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_question_relation` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -200,4 +229,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-03-19 19:35:07
+-- Dump completed on 2019-03-26 19:17:10
